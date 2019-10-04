a56c0618dee24df6e02b34f95225d712ea95991f
Martyna Skup <46429607+snikerslala@users.noreply.github.com>
2019-10-04 17:57:29 +0200

    Add sample user data

---
+++ src/userData.js
@@ -0,0 +1,60 @@
+export const useeData = [
+  {
+    id: 1,
+    userId: 1,
+    userName: 'Katrin Papelus',
+    emails: [
+      'user1@maindomain1.com',
+      'kahtrinpap@maindomain1.com',
+      'userus1@mainidomainit.co.uk',
+      'usir342.emailus.com'
+    ]
+  },
+  {
+    id: 2,
+    userId: 2,
+    userName: 'Steve Lincoln',
+    emails: [
+      'lincolniS@maindomain1.com',
+      'userus223@mainidomainit.co.uk',
+      'usir3jjf.emailmesteve.com',
+      'usir3jjf@emailmesteve.it.com'
+    ]
+  },
+  {
+    id: 3,
+    userId: 3,
+    userName: 'Paulo Colantonio',
+    emails: [
+      'paulo.colantonio4533244@maindomainico.com',
+      'user_colanto@mainidomainit.it.uk'
+    ]
+  },
+  {
+    id: 4,
+    userId: 4,
+    userName: 'Auto-Haus GmbH',
+    emails: [
+      'infomail@auto-hausus-mail.at',
+      'buearomail@auto-hausus-mail.at',
+      'questions_and_answers@auto-hausus-mail.at',
+      'no-reply@auto-hausus-mail.at'
+    ]
+  },
+  {
+    id: 5,
+    userId: 5,
+    userName: 'Ka Ki Ko',
+    emails: [
+      'kakiko12@maindomain2.com',
+      'kakakkokit.emailus.com',
+      'kakiko10@mainidomainit.com'
+    ]
+  },
+  {
+    id: 6,
+    userId: 6,
+    userName: 'Miki Maus',
+    emails: []
+  }
+];
