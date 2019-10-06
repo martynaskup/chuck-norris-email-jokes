a2bda44408dfe9226922c7ddbcc16f83282eb080
Martyna Skup <46429607+snikerslala@users.noreply.github.com>
2019-10-06 22:14:12 +0200

    Sort user emails by domain and name

--- src/components/emails/emailData.js
+++ src/components/emails/emailData.js
@@ -1,8 +1,8 @@
 const emailData = (id, users, userId) => {
-  const emailList = [];
-
   const RegExp = /^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]*[a-zA-Z0-9]))*[@](?:[a-zA-Z]+(?:([a-zA-Z0-9-]*[a-zA-Z0-9]))*\.)+[a-zA-Z]+(?:([a-zA-Z0-9-]*[a-zA-Z0-9]))*$/;
 
+  // list of valid email addresses
+  const emailList = [];
   users
     .filter(user => id === user.userId)
     .forEach(user => {
@@ -13,7 +13,27 @@ const emailData = (id, users, userId) => {
       });
     });
 
-  return emailList;
+  // group emails by domain
+  const groupedByDomain = emailList.reduce((groups, email) => {
+    const [account, domain] = email.split('@');
+
+    if (!groups[domain]) groups[domain] = [];
+
+    groups[domain].push(account);
+    return groups;
+  }, {});
+
+  // sort the emails grouped by domain and name
+  const arrayDomain = Object.keys(groupedByDomain).sort();
+  const sortByDomain = [];
+  for (let domain of arrayDomain) {
+    groupedByDomain[domain].sort();
+    groupedByDomain[domain].forEach(item =>
+      sortByDomain.push(item.concat('@', domain))
+    );
+  }
+
+  return sortByDomain;
 };
 
 export default emailData;
