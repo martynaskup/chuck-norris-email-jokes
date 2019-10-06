956c7c536714fd4d65eed6236a923a9ba0c10726
Martyna Skup <46429607+snikerslala@users.noreply.github.com>
2019-10-06 23:14:13 +0200

    Updates

---
+++ precache-manifest.0c66a99536765438bbf6c0cb3cad82eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5f8e0992e3a343317ba4a42623a068",
+    "url": "/chuck-norris-email-jokes/index.html"
+  },
+  {
+    "revision": "cad768415aba5d4ac6a5",
+    "url": "/chuck-norris-email-jokes/static/css/main.4f9f1923.chunk.css"
+  },
+  {
+    "revision": "799f2375916fffadfc4c",
+    "url": "/chuck-norris-email-jokes/static/js/2.b5a2300d.chunk.js"
+  },
+  {
+    "revision": "cad768415aba5d4ac6a5",
+    "url": "/chuck-norris-email-jokes/static/js/main.9a548b24.chunk.js"
+  },
+  {
+    "revision": "a02f34dba78c5235d906",
+    "url": "/chuck-norris-email-jokes/static/js/runtime-main.0d71022a.js"
+  }
+]);
